783dd7fdb290b625fe325bd6f63a03ae346e3c89
arhovumyam <aregar20012001@gmail.com>
2026-06-01 20:54:46 -0700

    hh

--- 5_UART_TX/Src/main.c
+++ 5_UART_TX/Src/main.c
@@ -1,38 +1,46 @@
 #include "stm32f4xx.h"
 
 #define GPIOAEN			(1U<<0) // put a 1 in position 0 0b00000001
-#define GPIOCEN			(1U<<2) // put a 1 in position 2 0b00000100
-
-
-#define PIN_5			(1U<<5) // same for 0b00100000
-#define LED_PIN 		PIN_5
-
-#define PIN_13			(1U<<13)
-#define BTN_PIN			(PIN_13)
+#define UART2EN			(1U<<17)
 
 
 int main(void){
 
-	//Enable clk access to GPIOA and GPIOC
-	// When push button is on, the led goes off
-	RCC->AHB1ENR |= GPIOAEN;// enable clock for GPIOA, so you can control your pins
-	RCC->AHB1ENR |= GPIOCEN;// enable clock for GPIOC, so you can control your pins
+	while (1){
 
-//  set PA5 as output pin
-	GPIOA->MODER |=  (1U<<10); //mode is used for setting the mode register
-	GPIOA->MODER &=~ (01U<<11);
-
-//  set PC13 as input pin
-	GPIOC->MODER &=~ (1U<<26);
-	GPIOC->MODER &=~ (1U<<27);
-
-	while(1){
-		// check if btn is pressed
-		if(GPIOC->IDR & BTN_PIN){
-		GPIOA->BSRR = (1U<<21);
-		}
-		else {
-		GPIOA->BSRR = LED_PIN;
-		}
 	}
+}
+
+void uart2_tx_init(void){
+	/******************Configure the UART GPIO pin************/
+
+	/*Enable clock access to GPIOA*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*Set PA2 mode to alternate function mode*/
+	GPIOA->MODER |=  (1U<<5);
+	GPIOA->MODER &=~ (1U<<4);
+
+	/*Set PA2 alternate function type to UART_TX(AF07)*/
+	GPIOA->AFR[0] &=~  (1U<<11);
+	GPIOA->AFR[0] |=   (1U<<10);
+	GPIOA->AFR[0] |=   (1U<<9);
+	GPIOA->AFR[0] |=   (1U<<8);
+
+	/******************Configure UART module*****************/
+	/*Enable clock access to uart2*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*Configure baudrate*/
+	uart_set_baudrate(USART2,)
+	/*Configure the transfer direction*/
+	/*Enable UART module*/
+}
+
+static void uart_set_baudrate(USART_TypeDef  *USARTx,uint32_t_PeriphClk, unit32_t_BaudRate){
+	USARTx->BRR = compute_uart_bd(PeriphClk, Baudrate);
+}
+
+static uint16_t compute_uart_bd(uint32_t_PeriphClk, unit32_t_BaudRate){
+	result ((PeriphClk + (BaudRate/2U))/BaudRate);
 }
