fb08c592171a72ebda23dc5702b1b8599a07217f
Arego11 <aregar20012001@gmail.com>
2026-06-06 18:08:31 -0700

    printf

---
+++ 5_UART_TX/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/core_cm4.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_version.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_compiler.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_gcc.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/mpu_armv7.h \
+ /Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/core_cm4.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_version.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_compiler.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/cmsis_gcc.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Include/mpu_armv7.h:
+/Users/aro/Documents/STM32/ChipHeaders/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
